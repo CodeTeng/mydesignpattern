7555c232b1d59c94ca64c07cadca49ab24f4968c
teng <teng2002823@outlook.com>
2022-09-08 17:16:48 +0800

    解释器模式

---
+++ src/main/java/com/lt/designpattern/interpreter/Main.java
@@ -0,0 +1,12 @@
+package com.lt.designpattern.interpreter;
+
+/**
+ * @description: 解释器模式
+ * @author: ~Teng~
+ * @date: 2022/9/8 17:15
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
